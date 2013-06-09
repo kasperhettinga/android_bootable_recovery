8c2ffb131d9ca242f1e690f53cbc2d7c4bb3afb1
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-06-09 09:53:41 +0200

    UI: Add power off option to menu
    Change-Id: I367d74c3cbe5670ed5480a68ed271643a1303ebe

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -30,6 +30,7 @@ char* MENU_ITEMS[] = { "reboot system now",
                        "mounts and storage",
                        "advanced",
                        "AROMA Filemanager",
+                       "power off",
                        NULL };
 
 void device_ui_init(UIParameters* ui_parameters) {
