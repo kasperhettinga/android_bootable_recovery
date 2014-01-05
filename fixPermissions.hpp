6053aeeedb1abe9dabef4e6dd59e7e3b2f0313db
bigbiff bigbiff <bigbiff@teamw.in>
2014-01-05 18:13:43 -0500

    Do a restore of loaded file contexts to /data/media directory. This will help denials to be prevented when internal storage is wiped.
    Change-Id: Ie49bbfa48d2f3ee4f08376d9c1877b92c051017c

--- fixPermissions.hpp
+++ fixPermissions.hpp
@@ -10,12 +10,14 @@
 #include <dirent.h>
 #include "gui/rapidxml.hpp"
 #include "twrp-functions.hpp"
+#include "data.hpp"
 
 using namespace std;
 
 class fixPermissions {
 	public:
 		int fixPerms(bool enable_debug, bool remove_data_for_missing_apps);
+		int fixDataInternalContexts(void);
 
 	private:
 		int pchown(std::string fn, int puid, int pgid);
@@ -27,8 +29,8 @@ class fixPermissions {
 		int fixDataApps();
 		int fixAllFiles(string directory, int gid, int uid, string file_perms);
 		int fixDataData(string dataDir);
-		int fixDataDataContexts(void);
 		int restorecon(std::string entry, struct stat *sb);
+		int fixDataDataContexts(void);
 
 		struct package {
 			string pkgName;
@@ -44,6 +46,6 @@ class fixPermissions {
 		bool remove_data;
 		bool multi_user;
 		package* head;
-		package* temp;		
+		package* temp;
 		string packageFile;
 };
